c0bb6b36276d7eaa83221cb0ac68f25ae9835399
zhangjingjian <zhangjingjian@gomo.com>
2020-04-27 14:20:19 +0800

    Initial Commit

---
+++ LEDPulseMeasure/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LEDPulseMeasure
+//
+//  Created by zjj on 2020/4/27.
+//  Copyright © 2020 zjj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LEDPulseMeasure/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LEDPulseMeasure
+//
+//  Created by zjj on 2020/4/27.
+//  Copyright © 2020 zjj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
